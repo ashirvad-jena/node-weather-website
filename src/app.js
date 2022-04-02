b080ea5ce920000cf9d20980a94b8ceb9b489bd3
Ashirvad Jena <ashirvad.jena@swiggy.in>
2022-04-02 12:06:33 +0530

    migrated html to hbs

--- src/app.js
+++ src/app.js
@@ -3,12 +3,28 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-  res.send('Hello express!!');
+  res.render('index', {
+    title: 'Weather',
+    name: 'Ashirvad',
+  });
 });
 
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'All about us',
+    name: 'Ashirvad',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'This is an example message',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
