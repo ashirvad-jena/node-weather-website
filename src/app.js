928bc61be848d1c14aad7ff240a5e21cbeec07c4
Ashirvad Jena <ashirvad.jena@swiggy.in>
2022-04-02 21:38:38 +0530

    styling added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -23,6 +34,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
   res.render('help', {
     message: 'This is an example message',
+    title: 'Help',
+    name: 'Ashirvad J',
   });
 });
 
@@ -30,6 +43,20 @@ app.get('/weather', (req, res) => {
   res.send({
     forecast: `It's cloudy`,
     location: 'bangalore',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('error404', {
+    message: 'Help article not found',
+    name: 'Ashirvad',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('error404', {
+    message: 'My 404 page',
+    name: 'Ashirvad',
   });
 });
 
